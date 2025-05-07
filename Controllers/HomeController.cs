102d0dad3fbe6edcc0ec04c18a04992fe390e34a
Bashat Parween <68892857+bashat450@users.noreply.github.com>
2025-05-07 17:42:19 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using MvcProducts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProducts.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<ProductModel> Pro = new List<ProductModel>()
+        {
+            new ProductModel()
+            {
+                Id = 1, Name = "Adidas Shoes", Picture = "~/images/Adidas.jpg", Price = 10000.0
+            },
+            new ProductModel()
+            {
+                Id = 2, Name = "Champion Shoes", Picture = "~/images/Champion.jpg", Price = 15000.0
+            },
+            new ProductModel()
+            {
+                Id = 3, Name = "Nike Shoes", Picture = "~/images/Nike.jpg", Price = 8000.0
+            },
+            new ProductModel()
+            {
+                Id = 4, Name = "U.S. Polo Shoes", Picture = "~/images/U.S. Polo.jpg", Price = 20000.0
+            }
+        };
+        public ActionResult Index()
+        {
+            return View(Pro);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
